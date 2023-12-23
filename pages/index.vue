1d5b04d55adee407739d0a13065c0cf23753080e
Larry W. <l422y@l422y.com>
2023-12-23 12:35:42 -0500

    Add welcome message for logged in users in index.vue

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <ClientOnly>
+            <div v-if="auth.isLoggedIn && auth.user">
+                <div class="fw-light text-lowercase">Welcome back, {{ auth.user?.email }}</div>
+            </div>
+        </ClientOnly>
+    </div>
+</template>
+<script lang="ts" setup>
+import { useAuthStore } from "~/stores/auth"
+
+const passed = withDefaults(defineProps<{}>(), {})
+const auth = useAuthStore()
+</script>
+<style lang="scss" scoped>
+
+</style>
