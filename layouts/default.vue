54aa354d6c2d85b2925e4cdb52903e4e35007e69
Larry W. <l422y@l422y.com>
2023-12-23 12:35:17 -0500

    Add default layout with AppHeader and NuxtPage components

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <AppHeader />
+    <main>
+        <NuxtPage/>
+    </main>
+</template>
+<script lang="ts" setup>
+import { useAuthStore } from "~/stores/auth"
+
+const auth = useAuthStore()
+</script>
+<style lang="scss" scoped>
+
+</style>
