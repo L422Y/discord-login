020f1d811f346b6c2c617f92e28c5ede799927a2
Larry W. <l422y@l422y.com>
2023-12-23 13:44:51 -0500

    Add styling for button and small tag in app.vue

--- app.vue
+++ app.vue
@@ -13,6 +13,17 @@ html {
     line-height: 1.5;
     color: #333;
     background-color: #fff;
+}
 
+button {
+    padding: 0.5rem 1rem;
+    cursor: pointer;
+    border: 1px solid #ccc;
+    border-radius: 0.25rem;
+    background-color: #000;
+    color: #fff;
+    small {
+        display: block;
+    }
 }
 </style>
