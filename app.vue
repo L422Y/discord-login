2090ce3a8da956df5d533f58f06a52ddee64f176
Larry W. <l422y@l422y.com>
2023-12-23 12:35:07 -0500

    Add new app.vue with NuxtLayout and session check, update html styles

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+    <NuxtLayout/>
+</template>
+<script lang="ts" setup>
+import { useAuthStore } from "#imports"
+
+useAuthStore().checkSession()
+</script>
+<style>
+html {
+    font-family: sans-serif;
+    font-size: 16px;
+    line-height: 1.5;
+    color: #333;
+    background-color: #fff;
+
+}
+</style>
